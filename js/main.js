da2e607404ec4e85d598d24af7521e6be054e2fe
alejandrovc23 <3liseyou@gmail.com>
2026-04-03 01:05:20 -0500

    Add country search functionality

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
 import { getAllCountries } from "./api.js";
 import { displayCountries } from "./display.js";
+import { searchCountries } from "./search.js";
 
 console.log("Country Explorer initialized");
+
+const searchInput = document.querySelector("#country-search");
+const searchButton = document.querySelector(".search-controls button");
+
+let allCountries = [];
 
 loadCountries();
 
 async function loadCountries() {
   const countries = await getAllCountries();
   console.log(countries);
-  displayCountries(countries);
+
+  allCountries = countries ?? [];
+  displayCountries(allCountries);
+}
+
+searchButton?.addEventListener("click", handleSearch);
+
+function handleSearch() {
+  const query = searchInput?.value ?? "";
+  const filteredCountries = searchCountries(allCountries, query);
+  displayCountries(filteredCountries);
 }
