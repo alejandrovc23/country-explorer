42cbdfb4f6f8d00e9afa0b735fd0946bb821a2e9
alejandrovc23 <3liseyou@gmail.com>
2026-04-03 00:45:27 -0500

    Fix REST Countries API fields query

--- js/api.js
+++ js/api.js
@@ -1,1 +1,11 @@
-export {};
+export async function getAllCountries() {
+  try {
+    const response = await fetch(
+      "https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags"
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching countries:", error);
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+import { getAllCountries } from "./api.js";
+
 console.log("Country Explorer initialized");
+
+loadCountries();
+
+async function loadCountries() {
+  const countries = await getAllCountries();
+  console.log(countries);
+}
