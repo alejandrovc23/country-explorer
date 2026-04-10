036e8ddfa96b0d035b63ef70b2a2cce9e24290b1
alejandrovc23 <3liseyou@gmail.com>
2026-04-10 00:53:05 -0500

    Add country sorting feature

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 export async function getAllCountries() {
   try {
     const response = await fetch(
-      "https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags"
+      "https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags,cca2,cca3"
     );
     const data = await response.json();
     return data;
